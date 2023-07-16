2e613b74444fde909c71425b02e23a2d7101e2e2
Yorí Galisteu <yorigcsdev@gmail.com>
2023-07-16 19:01:22 -0300

    feat: create user resources

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, Length } from 'class-validator';
+
+export class CreateUserDto {
+  @Length(4, 20)
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @Length(8, 40)
+  password: string;
+
+  @Length(8, 40)
+  confirmPassword: string;
+}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Post, Body, HttpException, HttpStatus } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('User')
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('create')
+  @ApiResponse({
+    status: 409,
+    description: 'This e-mail is already associated with an account',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Invalid field validation',
+  })
+  create(@Body() createUserDto: CreateUserDto) {
+    const { password, confirmPassword } = createUserDto;
+    if (password !== confirmPassword) {
+      throw new HttpException(
+        'The fields password and confirm password must be equals.',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return this.userService.create(createUserDto);
+  }
+}
