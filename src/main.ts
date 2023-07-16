f34886713f69b5deb34604696a015980668eed64
Yorí Galisteu <yorigcsdev@gmail.com>
2023-07-16 16:53:25 -0300

    feat: implements cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.use(helmet());
   await app.listen(3000);
 }
