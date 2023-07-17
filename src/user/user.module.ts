bb5cbf7001ccaac71c9b60cdaf30cdb7037c457b
Yorí Galisteu <yorigcsdev@gmail.com>
2023-07-17 00:26:12 -0300

    refactor: hash password before create user.

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { userProviders } from './user.providers';
 import { DatabaseModule } from '../database.modules';
+import { HashModule } from '../shared/hash/hash.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, HashModule],
   controllers: [UserController],
   providers: [...userProviders, UserService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,15 +3,17 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { UserEmailError } from './errors/user.email.error';
+import { HashService } from '../shared/hash/hash.service';
 
 @Injectable()
 export class UserService {
-  constructor(@Inject('USER_REPOSITORY') private userRepository: Repository<User>) {}
+  constructor(@Inject('USER_REPOSITORY') private readonly userRepository: Repository<User>, private readonly hashService: HashService) {}
   async create(createUserDto: CreateUserDto) {
     const { email, password, username } = createUserDto;
     const user = await this.userRepository.findOneBy({ email });
     if (user !== null) throw new UserEmailError();
-    await this.userRepository.save({ email, password, username });
+    const hashedPassword = await this.hashService.hash(password);
+    await this.userRepository.save({ email, password: hashedPassword, username });
     return 'User created!';
   }
 }
