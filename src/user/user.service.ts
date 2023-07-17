c3a738f101098916ea2e675fd41650f447940cd7
Yorí Galisteu <yorigcsdev@gmail.com>
2023-07-16 23:14:13 -0300

    feat: implements User service method create

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { UserEmailError } from './errors/user.email.error';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@Inject('USER_REPOSITORY') private userRepository: Repository<User>) {}
+  async create(createUserDto: CreateUserDto) {
+    const { email, password, username } = createUserDto;
+    const user = await this.userRepository.findOneBy({ email });
+    if (user !== null) throw new UserEmailError();
+    await this.userRepository.save({ email, password, username });
+    return 'User created!';
   }
 }
