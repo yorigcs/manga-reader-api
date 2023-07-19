e553ae22b0b1c6effa4d0b3f27673a252ea46d78
Yorí Galisteu <yorigcsdev@gmail.com>
2023-07-19 03:46:34 -0300

    feat: implements hash compare

--- src/contracts/cryptography/hash.ts
+++ src/contracts/cryptography/hash.ts
@@ -1,3 +1,7 @@
 export interface Hash {
   hash: (plainText: string) => Promise<string>;
 }
+
+export interface HashCompare {
+  compare: (plainText: string, cipherText) => Promise<boolean>;
+}
--- src/shared/hash/hash.service.ts
+++ src/shared/hash/hash.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
-import { Hash } from '../../contracts/cryptography/hash';
+import { Hash, HashCompare } from '../../contracts/cryptography/hash';
 
 @Injectable()
-export class HashService implements Hash {
+export class HashService implements Hash, HashCompare {
   async hash(plainText: string) {
     const salt = 10;
     return await bcrypt.hash(plainText, salt);
+  }
+
+  async compare(plainText: string, cipherText): Promise<boolean> {
+    return bcrypt.compare(plainText, cipherText);
   }
 }
