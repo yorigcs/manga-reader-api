ad46d893b46672119a100129bbe589d0e816ed33
Yorí Galisteu <yorigcsdev@gmail.com>
2023-07-20 20:06:48 -0300

    refactor: apply Command Pattern to hash

--- src/contracts/cryptography/hash.ts
+++ src/contracts/cryptography/hash.ts
@@ -1,7 +1,22 @@
 export interface Hash {
-  hash: (plainText: string) => Promise<string>;
+  hash: (input: Hash.Input) => Promise<Hash.Output>;
+}
+
+export namespace Hash {
+  export type Input = {
+    plainText: string;
+  };
+  export type Output = string;
 }
 
 export interface HashCompare {
-  compare: (plainText: string, cipherText) => Promise<boolean>;
+  compare: (input: HashCompare.Input) => Promise<HashCompare.Output>;
+}
+
+export namespace HashCompare {
+  export type Input = {
+    plainText: string;
+    cipherText: string;
+  };
+  export type Output = boolean;
 }
--- src/shared/hash/hash.service.ts
+++ src/shared/hash/hash.service.ts
@@ -4,12 +4,12 @@ import { Hash, HashCompare } from '../../contracts/cryptography/hash';
 
 @Injectable()
 export class HashService implements Hash, HashCompare {
-  async hash(plainText: string) {
+  async hash({ plainText }: Hash.Input): Promise<Hash.Output> {
     const salt = 10;
     return await bcrypt.hash(plainText, salt);
   }
 
-  async compare(plainText: string, cipherText): Promise<boolean> {
+  async compare({ plainText, cipherText }: HashCompare.Input): Promise<HashCompare.Output> {
     return bcrypt.compare(plainText, cipherText);
   }
 }
