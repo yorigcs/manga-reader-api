c3cecefb50599e3e5e9a8faee3a87364ef478c8f
Yorí Galisteu <yorigcsdev@gmail.com>
2023-07-21 19:56:46 -0300

    feature: add ConflictException

--- src/tags/tags.service.ts
+++ src/tags/tags.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateTagDto } from './dto/create-tag.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Tag } from './entities/tag.entity';
@@ -9,6 +9,8 @@ export class TagsService {
   constructor(@InjectRepository(Tag) private readonly tagsRepo: Repository<Tag>) {}
   async create(createTagDto: CreateTagDto) {
     const { name } = createTagDto;
+    const tag = await this.tagsRepo.findOneBy({ name });
+    if (tag !== null) throw new ConflictException('This tag name already exists!');
     await this.tagsRepo.save({ name });
     return 'Tag created!';
   }
