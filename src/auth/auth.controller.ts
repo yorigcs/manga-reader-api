ea40d713093ebc43f79da26d2609d6f4270dbda8
Yorí Galisteu <yorigcsdev@gmail.com>
2023-07-22 16:35:56 -0300

    docs: add swagger bad request response to auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 import { AuthModelResponse } from './swagger/auth.model';
@@ -10,6 +10,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @Post()
   @ApiCreatedResponse({ type: AuthModelResponse })
+  @ApiBadRequestResponse({ description: "This email and password doesn't match" })
   async AuthWithDefault(@Body() authDto: AuthDto) {
     return await this.authService.withDefault(authDto);
   }
