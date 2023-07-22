07259a086d7479f64c92765cc0ba002ae982a13e
Yorí Galisteu <yorigcsdev@gmail.com>
2023-07-21 23:49:54 -0300

    chore: create s3 env config

--- src/configuration/env.config.ts
+++ src/configuration/env.config.ts
@@ -10,4 +10,10 @@ export default () => ({
   jwt: {
     accessTokenSecret: process.env.JWT_ACCESS_TOKEN_SECRET,
   },
+  s3: {
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    region: process.env.S3_REGION,
+    bucket: process.env.S3_BUCKET,
+  },
 });
