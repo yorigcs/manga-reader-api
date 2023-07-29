ba50e8ab3225d3fa3e448c4f477155dc6d6e750c
Yorí Galisteu <yorigcsdev@gmail.com>
2023-07-29 03:22:48 -0300

    refactor: remove unnecessary decorator

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,21 +1,17 @@
-import { IsArray, IsIn, IsNotEmpty, IsString } from 'class-validator'
+import { IsArray, IsIn, IsString } from 'class-validator'
 import { type UserPayload } from '../../auth/auth.service'
 import { BookType } from '../swagger/books.model'
 
 export class CreateBookDto {
-  @IsNotEmpty()
   @IsString()
     title: string
 
-  @IsNotEmpty()
   @IsString()
     synopsis: string
 
-  @IsNotEmpty()
   @IsString()
     author: string
 
-  @IsNotEmpty({ each: true })
   @IsString({ each: true })
   @IsArray()
     tags: string[]
