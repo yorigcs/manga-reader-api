a2a70e225aa2a7c95a534e3879d2bed0de3f6224
Yorí Galisteu <yorigcsdev@gmail.com>
2023-07-29 03:24:58 -0300

    feat: create search books service

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -4,6 +4,7 @@ import { AWS_PROVIDES } from '../constants'
 import { AwsS3FileStorageService } from '../shared/storage/aws-s3-file-storage.service'
 import { UuidService } from '../shared/uuid/uuid.service'
 import { PrismaService } from '../prisma.service'
+import { type SearchBooksDto } from './dto/search-books.dto'
 
 @Injectable()
 export class BooksService {
@@ -19,7 +20,7 @@ export class BooksService {
     if (book !== null) throw new ConflictException('This book already exists!')
     const availableTags = (await this.prisma.tag.findMany()).map(tag => tag.name)
     for (const tag of tags) {
-      if (!availableTags.includes(tag)) throw new BadRequestException(`[${tag}] is not a available tag. Available tags: [${availableTags.toString() as string}]`)
+      if (!availableTags.includes(tag)) throw new BadRequestException(`[${tag}] is not a available tag. Available tags: [${availableTags.toString()}]`)
     }
     const extension = mimetype.split('/')[1]
     const bookImage = await this.s3.upload({ file: buffer, fileName: `${this.uuidService.generate({ key: 'cover' })}.${extension}` })
@@ -27,7 +28,25 @@ export class BooksService {
     return 'Book created with success'
   }
 
-  async findAllBooksWithNChapters () {
-    return this.prisma.book.findMany()
+  async searchBooks ({ page, booksPerPage, allowedTags, bannedTags, withLastChapters }: SearchBooksDto) {
+    const takeBooks = booksPerPage ?? 10
+    const skipBooks = page ? (page - 1) * takeBooks : 0
+    const [books, booksCount] = await this.prisma.extended.book.findManyAndCount({
+      skip: skipBooks,
+      take: takeBooks,
+      orderBy: { updatedAt: 'desc' },
+      where: {
+        tags: allowedTags ? { hasSome: allowedTags } : undefined,
+        NOT: bannedTags ? { tags: { hasSome: bannedTags } } : undefined
+      },
+      include: withLastChapters
+        ? { chapters: { orderBy: { chapterNum: 'desc' }, take: withLastChapters } }
+        : undefined
+    })
+    const availablePages = Math.ceil(booksCount / takeBooks)
+    const currentPage = page ?? 1
+    const previousPage = (currentPage - 1) > 0 && (currentPage - 1) < availablePages ? (currentPage - 1) : null
+    const nextPage = (currentPage + 1) <= availablePages ? (currentPage + 1) : null
+    return { books, booksCount, availablePages, currentPage, previousPage, nextPage }
   }
 }
